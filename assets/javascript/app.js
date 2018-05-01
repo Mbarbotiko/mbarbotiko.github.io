cc92c80846d4220bc8c7ea50eeff35240ef50c55
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-04-30 21:08:07 -0500

    a new look

---
+++ assets/javascript/app.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+
+    $("#about-icon").hover(function(){
+        $(this).animate({ 
+          width: '150px',
+        }, 1000, );
+        $(this).animate({ 
+            width: '120px',
+          }, 1000 );
+      });
+
+      
+    $("#portfolio-icon").hover(function(){
+        $(this).animate({ 
+          width: '130px',
+        }, 1000, );
+        $(this).animate({ 
+            width: '100px',
+          }, 1000 );
+      });
+
+      
+    $("#contact-icon").hover(function(){
+        $(this).animate({ 
+          width: '130px',
+        }, 1000, );
+        $(this).animate({ 
+            width: '100px',
+          }, 1000 );
+      });
+
+//add an onclick or hover for my image for a short message welcoming the user to my page
+
+
+
+  var config = {
+    apiKey: "AIzaSyC4f8X5KLqYfuzT9ayM8pgWB0f-1UDGJpY",
+    authDomain: "portfolio-submit.firebaseapp.com",
+    databaseURL: "https://portfolio-submit.firebaseio.com",
+    projectId: "portfolio-submit",
+    storageBucket: "https://portfolio-submit.appspot.com",
+    messagingSenderId: "1087976841086"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+
+  $('#submitBtn').one('click', function (event) {
+      event.preventDefault();
+      var name = $('#name').val().trim();
+      var email = $('#email').val().trim();
+      var userText = $('#user-entry').val().trim();
+
+      var newSubmit = {
+          enteredName: name,
+          enteredEmail: email,
+          enteredText: userText,
+      };
+
+      database.ref().push(newSubmit);
+
+
+      $("#name").val("");
+      $("#email").val("");
+      $("#user-entry").val("");
+
+      alert('Thanks!');
+      //use empty to clear these out
+
+
+
+  });
+
+
+
+});
+
