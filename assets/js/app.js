601aaa212b4f053cf8d178b2979d352d1c443f1d
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-05-14 06:35:16 -0500

    fixed table issue on contact, added github linked in and twitter links to main page and contact page

--- assets/js/app.js
+++ assets/js/app.js
@@ -96,14 +96,25 @@ $(document).ready(function () {
 <div class="card" >
 <div class="card-header">
   Contact Information:
-</div>
+</div><div>
 <ul class="list-group list-group-flush">
   <li class="list-group-item">Margarita Barbotiko</li>
   <li class="list-group-item">mbarbotiko@gmail.com</li>
   <li class="list-group-item">651-600-1396</li>
-  <a li class="list-group-item" href="assets/MargaritaBarbotiko2018.pdf" target="_blank">Resume</li>
+  <li class="list-group-item"> <a href="assets/MargaritaBarbotiko2018.pdf" target="_blank">Resume</a></li>
+</ul>	</div><div>
+<ul class="icons">
+  <li>
+    <a href="https://twitter.com/mbarbotiko1"  target="_blank" class="fa-twitter">Twitter</a>
+  </li>
+  <li>
+    <a href="https://www.linkedin.com/in/margarita-barbotiko-b5814050/"  target="_blank" class="fa-linkedin">Instagram</a>
+  </li>
+  <li>
+    <a href="https://github.com/Mbarbotiko" target="_blank" class="fa-github">Facebook</a>
+  </li>
 </ul>
-</div>`)
+</div>`);
   }
 
 
