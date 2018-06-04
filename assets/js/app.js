fe554fe81df1cbd163b908c1c51c9bbfaf452d2a
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-06-03 20:38:27 -0500

    adding bamazon video link, screenshot, and rep link

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,44 +53,59 @@ $(document).ready(function () {
       <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
   </ol><div class="carousel-inner">
+
   <div class="carousel-item active">
-  <img class="d-block w-100" src="images/triviagame.jpg"href="https://mbarbotiko.github.io/TriviaGame/"target="_blank" alt="Second slide">
+  <img class="d-block w-100" src="images/triviagame.jpg" alt="First slide">
   <a href="https://mbarbotiko.github.io/TriviaGame/" target="_blank"><h4>(APP) Video Game Trivia (JQuery)</h4></a>
 <a href="https://github.com/Mbarbotiko/TriviaGame" target="_blank"><h4>(GitHub Repo)</h4></a>
   </div>
+
   <div class="carousel-item">
-      <img class="d-block w-100" src="images/crystalsgame.jpg"href="https://mbarbotiko.github.io/Week-4-Game/"target="_blank" alt="Second slide">
+      <img class="d-block w-100" src="images/crystalsgame.jpg" alt="Second slide">
       <a href="https://mbarbotiko.github.io/Week-4-Game/" target="_blank"><h4>(APP) Crystals Game (JQuery)</h4></a>
   <a href="https://github.com/Mbarbotiko/Week-4-Game" target="_blank"><h4>(GitHub Repo)</h4></a>
   </div>
 
 
   <div class="carousel-item">
-      <img class="d-block w-100" src="images/api.jpg"href="https://mbarbotiko.github.io/API/"target="_blank" alt="Third slide">
+      <img class="d-block w-100" src="images/api.jpg" alt= "Third slide">
       <a href="https://mbarbotiko.github.io/API/" target="_blank"><h4>(APP)Cartoon Giphy (Querying API)</h4></a><a href="https://github.com/Mbarbotiko/API" target="_blank"><h4>(GitHub Repo)</h4></a>
   </div>
+
   <div class="carousel-item">
-      <img class="d-block w-100" src="images/trainschedule.JPG" href="https://mbarbotiko.github.io/Train-Schedule/"target="_blank"alt="Fourth slide">
+      <img class="d-block w-100" src="images/trainschedule.JPG" alt="Fourth slide">
       <a href="https://mbarbotiko.github.io/Train-Schedule/" target="_blank"><h4>(APP)Train Schedule (Firebase)</h4></a><a href="https://github.com/Mbarbotiko/Train-Schedule" target="_blank"><h4>(GitHub Repo)</h4></a>
   </div>
+
   <div class="carousel-item">
-      <img class="d-block w-100" src="images/donna.jpg" href="https://dbarthell.github.io/GroupProject1/"target="_blank"alt="Fifth slide">
+      <img class="d-block w-100" src="images/donna.jpg" alt="Fifth slide">
       <a href="https://dbarthell.github.io/GroupProject1/" target="_blank"><h4>(APP)Travel with Donna</h4></a><a href="https://github.com/dbarthell/GroupProject1" target="_blank"><h4>(GitHub Repo)</h4></a>
   </div>
+
   <div class="carousel-item">
-  <img class="d-block w-100" src="images/group-project1.JPG" href="https://dbarthell.github.io/GroupProject1/"target="_blank"alt="Fifth slide">
+  <img class="d-block w-100" src="images/group-project1.JPG" alt="Sixth slide">
   <a href="https://codingbootcamp.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=60efee95-091b-4888-9a88-a8d7013a46fc" target="_blank"><h4>(APP)Travel with Donna DEMO Video</h4></a>
 </div>
+
 <div class="carousel-item">
-<img class="d-block w-100" src="images/liri.JPG" href="https://dbarthell.github.io/GroupProject1/"target="_blank"alt="Fifth slide">
+<img class="d-block w-100" src="images/liri.JPG" alt="Seventh slide">
 <a href="https://github.com/Mbarbotiko/liri-node-app" target="_blank"><h4>(Node.JS)Liri Bot(GitHub Repo)</h4></a>
 <a href="https://drive.google.com/file/d/1EparZQQIjtx0XB9PTWqC_IH6sS07MWPJ/view?usp=sharing
 " target="_blank"><h4>Liri Bot (Video)</h4></a>
+</div>
 
-
+<div class="carousel-item">
+<img class="d-block w-100" src="images/bamazon.jpg" alt="Eighth slide">
+<a href="https://github.com/Mbarbotiko/bamazon" target="_blank"><h4>(Node.JS)Bamazon(GitHub Repo)</h4></a>
+<a href="https://drive.google.com/file/d/1dRfkBjcGQ7oKHhpowLm6AbaoYvssZapD/view
+" target="_blank"><h4>Bamazon (Video)</h4></a>
+</div>
 
 </div>
-</div>
+
+
+
+
 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
   <span class="sr-only">Previous</span>
