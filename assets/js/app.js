a4b80d640cb1a7f700faecf2c042497a6b9b9138
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-08-25 21:40:07 -0500

    ready to merge, work out details later

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,3 +26,19 @@ $(document).ready(function () {
 
 });
 
+// https://your-bestfriend-finder.herokuapp.com/
+// https://github.com/Mbarbotiko/FriendFinder
+// https://github.com/Mbarbotiko/bamazon
+// https://drive.google.com/file/d/1dRfkBjcGQ7oKHhpowLm6AbaoYvssZapD/view
+// https://github.com/Mbarbotiko/liri-node-app
+// https://drive.google.com/file/d/1EparZQQIjtx0XB9PTWqC_IH6sS07MWPJ/view
+// https://github.com/dbarthell/GroupProject1
+// https://dbarthell.github.io/GroupProject1/
+// https://github.com/Mbarbotiko/Train-Schedule
+// https://mbarbotiko.github.io/Train-Schedule/
+// https://github.com/Mbarbotiko/API
+// https://mbarbotiko.github.io/API/
+// https://github.com/Mbarbotiko/Week-4-Game
+// https://mbarbotiko.github.io/Week-4-Game/
+// https://github.com/Mbarbotiko/TriviaGame
+// https://mbarbotiko.github.io/TriviaGame/
