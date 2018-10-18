662c0adb66038d8ad7db113c4d7ecbd28d0bf87d
Margarita Barbotiko <mbarbotiko@gmail.com>
2018-10-18 07:59:51 -0500

    adding dynamically created content removing HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,11 +13,13 @@ $(document).ready(function () {
     }
 
   }
-
+  //alt
 
   const projectList = [
     {
       header: 'Cava',
+      imageSource: './images/cava.jpg',
+      alt: 'Cava Restaurant Website',
       title: 'Front End Development',
       text: 'HTML|CSS|JQuery',
       footer: 'August 2018',
@@ -26,6 +28,8 @@ $(document).ready(function () {
     },
     {
       header: 'Neighborly|Group Project',
+      imageSource: './images/neighborly.jpg',
+      alt: 'Social Website',
       title: 'Back End Development',
       text: 'MongoDB|Mongoose|Express|React|ES6',
       footer: 'August 2018',
@@ -34,52 +38,165 @@ $(document).ready(function () {
     },
     {
       header: 'Poke-Switch',
+      imageSource: './images/pokemonswitch.jpg',
+      alt: 'Poke Switch Game',
       title: 'Front End Development',
       text: 'JSX|ES6|CSS|React',
       footer: 'August 2018',
       githubLink: 'https://github.com/Mbarbotiko/pokeswitch',
       projectLink: 'https://morning-mesa-94984.herokuapp.com/',
     },
+    {
+      header: 'SwapMee+Up|Group Project',
+      imageSource: './images/swapmeetup.jpg',
+      alt: 'SwapMeetup Website',
+      title: 'Back End Development',
+      text: 'MySQL|Sequelize|Express|Node',
+      footer: 'June 2018',
+      githubLink: 'https://github.com/Mbarbotiko/swapmeetup',
+      projectLink: 'https://swapmeetup.herokuapp.com/',
+    },
+    {
+      header: 'Friend Finder',
+      imageSource: './images/friendfinder.jpg',
+      alt: 'Friend Finder App',
+      title: 'Front & Back End Development',
+      text: 'HTML|CSS|JQuery|Express|Node',
+      footer: 'June 2018',
+      githubLink: 'https://github.com/Mbarbotiko/FriendFinder',
+      projectLink: 'https://your-bestfriend-finder.herokuapp.com/',
+    },
+    {
+      header: 'Train Schedule',
+      imageSource: './images/trainschedule.jpg',
+      alt: 'Train Schedule App',
+      title: 'Front End Development',
+      text: 'HTML|CSS|JQuery|Firebase',
+      footer: 'May 2018',
+      githubLink: 'https://github.com/Mbarbotiko/Train-Schedule',
+      projectLink: 'https://mbarbotiko.github.io/Train-Schedule/',
+    },
+    {
+      header: 'Travel with Donna|Group Project',
+      imageSource: './images/donna.jpg',
+      alt: 'Travel with Donna App',
+      title: 'Front End Development',
+      text: 'HTML|CSS|JQuery|API|AJAX',
+      footer: 'May 2018',
+      githubLink: 'https://github.com/dbarthell/GroupProject1',
+      projectLink: 'https://dbarthell.github.io/GroupProject1/',
+    },
+    {
+      header: 'Bamazon',
+      imageSource: './images/bamazon.jpg',
+      alt: 'Bamazon Store App',
+      title: 'Back End Development',
+      text: 'Node JS',
+      footer: 'May 2018',
+      githubLink: 'https://github.com/Mbarbotiko/bamazon',
+      projectLink: 'https://drive.google.com/file/d/1dRfkBjcGQ7oKHhpowLm6AbaoYvssZapD/view',
+    },
+    {
+      header: 'LiriBot',
+      imageSource: './images/liri.JPG',
+      alt: 'LiriBot App',
+      title: 'Back End Development',
+      text: 'Node JS',
+      footer: 'May 2018',
+      githubLink: 'https://github.com/Mbarbotiko/liri-node-app',
+      projectLink: 'https://drive.google.com/file/d/1EparZQQIjtx0XB9PTWqC_IH6sS07MWPJ/view',
+    },
+    {
+      header: 'Cartoon GIPHY',
+      imageSource: './images/api.jpg',
+      alt: 'Cartoon Gif App',
+      title: 'Front End Development',
+      text: 'HTML|CSS|JQuery|API|AJAX',
+      footer: 'April 2018',
+      githubLink: 'https://github.com/Mbarbotiko/API',
+      projectLink: 'https://mbarbotiko.github.io/API/',
+    },
+
+    {
+      header: 'Got Game Trivia',
+      imageSource: './images/triviagame.jpg',
+      alt: 'Got Game Trivia App',
+      title: 'Front End Development',
+      text: 'HTML|CSS|JQuery',
+      footer: 'April 2018',
+      githubLink: 'https://github.com/Mbarbotiko/TriviaGame',
+      projectLink: 'https://mbarbotiko.github.io/TriviaGame/',
+    },
+    {
+      header: 'Crystal Game',
+      imageSource: './images/crystalsgame.jpg',
+      alt: 'Crystal Game',
+      title: 'Front End Development',
+      text: 'HTML|CSS|Javascript|JQuery',
+      footer: 'March 2018',
+      githubLink: 'https://github.com/Mbarbotiko/Week-4-Game',
+      projectLink: 'https://mbarbotiko.github.io/Week-4-Game/',
+    },
+    // {
+    //   header: ' ',
+    //   imageSource: ' ',
+    //   alt: ' ',
+    //   title: ' ',
+    //   text: ' ',
+    //   footer: ' ',
+    //   githubLink: ' ',
+    //   projectLink: ' ',
+    // },
+
 
   ]
 
 
-  // function printProjectList() {
-  //   return projectList.map(function (print) {
-  //     return print.header + 'Header' + print.title + 'Title'
-  //   })
-  // }
-  // console.log(printProjectList());
+  function printProjectList() {
+    projectList.map(function (print) {
+      $('.project-list').append(
 
-//   function mapIt() {
-//     projectList.map(function (iterate) {
-//       console.log(iterate.header + iterate.title);
-//     })
-//   }
+        `<div class="inner-container"></div>
+        <div class="card">
+					<div class="project-card-container">
+							<a href=`+ print.projectLink + `target="_blank">
+						<img src=`+ print.imageSource + ` alt=` + print.alt + `>
+						</a>
+						<div class="card text-center project-card">
+							<div class="card-header">
+							`+ print.header + `
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">`+ print.title + `</h5>
+								<p class="card-text">`+ print.text + `</p>
+							</div>
+							<div class="card-footer text-muted">
+								`+ print.footer + `</div>
+						</div>
+						<div class="overlay-button">
+							<div class="github-container">
+								<a class="github-link" href=`+ print.githubLink + ` target="_blank">
+									<i class="fab fa-github fa-3x"></i>
+									<h2>Repository</h2>
+								</a>
+							</div>
+							<div class="projectlink-container">
+								<a class="project-link" href= `+ print.projectLink + ` target="_blank">
+									<i class="fas fa-book-open fa-2x"></i>
+									<h2>Live Project</h2>
+								</a>
+							</div>
+						</div>
+						<div class="overlay" href=""></div>
+					</div>
+				</div>
+			</div>`
 
-// mapIt();
+      )
+    })
+  }
 
-
-
-
-
-
+  printProjectList();
 
 });
 
-// https://your-bestfriend-finder.herokuapp.com/
-// https://github.com/Mbarbotiko/FriendFinder
-// https://github.com/Mbarbotiko/bamazon
-// https://drive.google.com/file/d/1dRfkBjcGQ7oKHhpowLm6AbaoYvssZapD/view
-// https://github.com/Mbarbotiko/liri-node-app
-// https://drive.google.com/file/d/1EparZQQIjtx0XB9PTWqC_IH6sS07MWPJ/view
-// https://github.com/dbarthell/GroupProject1
-// https://dbarthell.github.io/GroupProject1/
-// https://github.com/Mbarbotiko/Train-Schedule
-// https://mbarbotiko.github.io/Train-Schedule/
-// https://github.com/Mbarbotiko/API
-// https://mbarbotiko.github.io/API/
-// https://github.com/Mbarbotiko/Week-4-Game
-// https://mbarbotiko.github.io/Week-4-Game/
-// https://github.com/Mbarbotiko/TriviaGame
-// https://mbarbotiko.github.io/TriviaGame/
